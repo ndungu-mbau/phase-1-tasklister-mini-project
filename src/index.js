ee1cd4d47319d14616f0c04742df69e7bab0fe63
Nelson Mbau <mbau.ndungu@gmail.com>
2024-03-28 14:56:47 +0300

    feat: add event listeners for form submit to add todos

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+const tasks = [];
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("#create-task-form");
+  const list = document.querySelector("#tasks");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const description = document.querySelector("#new-task-description");
+    if (description.value) {
+      tasks.push(description.value);
+      description.value = "";
+      list.innerHTML = "";
+      tasks.forEach((task) => {
+        const li = document.createElement("li");
+        li.textContent = task;
+        list.appendChild(li);
+      });
+    }
+  });
 });
